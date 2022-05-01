7676a89f65fae3b46c4f39c13a05a4a20ecb8ae6
Viktoriia <shevictory@gmail.com>
2022-05-01 19:47:27 +0300

    Add update_contact action+creator

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -4,3 +4,9 @@ export const createContact = newContact => ({
   type: ACTION_TYPES.CREATE_CONTACT,
   newContact,
 });
+
+export const updateContact = (newContactInfo, contactId) => ({
+  type: ACTION_TYPES.UPDATE_CONTACT,
+  newContactInfo,
+  contactId,
+});
