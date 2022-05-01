d5057f6df934814b17c78f2df2354277ed60f1a6
Viktoriia <shevictory@gmail.com>
2022-05-01 17:01:37 +0300

    Add contactsReducer

---
+++ src/reducers/constactsReducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+  contacts: [
+    {
+      name: 'Test',
+      telNumber: '+380123456789',
+      birthday: '2000-12-01',
+      isFavourite: false,
+    },
+  ],
+  isFetching: false,
+  error: null,
+};
+
+const contactsReducer = (state, action) => {
+  const { type } = action;
+  switch (type) {
+    default:
+      return state;
+  }
+};
+
+export default contactsReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,21 @@
 import { combineReducers } from 'redux';
+import contactsReducer from './constactsReducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  contactsData: contactsReducer,
+});
 
 export default rootReducer;
+
+// state = {
+//   contactsData: {contacts: [
+//     {
+//       name: 'Test',
+//       telNumber: '+380123456789',
+//       birthday: '2000-12-01',
+//       isFavourite: false,
+//     },
+//   ],
+//   isFetching: false,
+//   error: null,},
+// };
